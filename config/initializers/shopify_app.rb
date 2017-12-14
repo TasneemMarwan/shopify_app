52127e3a4313410d7dcb54ed6c07851163b56a19
TasneemMarwan <tasneem@rubikal.com>
2017-12-14 14:52:28 +0200

    generate shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "308dcfcfbe6bd5f267aeb5158ca7a491"
+  config.secret = "b05c24a27975e10cb392e22ddd617f40"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
