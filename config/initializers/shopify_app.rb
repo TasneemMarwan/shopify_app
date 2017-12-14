258cba490b2b5a54f57182a831b5be1728298461
TasneemMarwan <tasneem@rubikal.com>
2017-12-14 14:56:12 +0200

    update redirect_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "308dcfcfbe6bd5f267aeb5158ca7a491"
   config.secret = "b05c24a27975e10cb392e22ddd617f40"
+  config.redirect_uri = "http://shopifygem.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
